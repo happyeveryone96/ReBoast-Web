3f074e61d40a90ecc8e4b3e8839a0e7cc3250875
happyeveryone96 <happyeveryone@naver.com>
2023-08-07 17:21:45 +0900

    feat: 메인 페이지에서 강의 카테고리 이동 버튼 클릭 시 강의 상세 페이지 필터링 기능 구현

--- src/app/pages/Home/Home.js
+++ src/app/pages/Home/Home.js
@@ -173,7 +173,7 @@ const Home = () => {
         <div className="new-lecture-text">신규 런칭 강의</div>
         <div className="new-lecture-box">
           <div className="last-week-best">최근 일주일 동안 많이 찾은 강의</div>
-          <Link to="/lecture">
+          <Link to={'/lecture'} state={{ filter: 'new' }}>
             <img
               className="arrow-right"
               src="/images/arrowRight.png"
@@ -237,7 +237,7 @@ const Home = () => {
         <div className="hot-lecture-text">인기 급상승 강의</div>
         <div className="hot-lecture-box">
           <div className="last-week-best">최근 일주일 동안 많이 찾은 강의</div>
-          <Link to="/lecture">
+          <Link to={'/lecture'} state={{ filter: 'hot' }}>
             <img
               className="arrow-right"
               src="/images/arrowRight.png"
