b62ba3ee0d5ad696c247e2f401a9ccfbacdc1348
happyeveryone96 <happyeveryone@naver.com>
2023-08-16 14:44:18 +0900

    design: 강의 카드 스타일 수정

--- src/app/components/DummyCard/DummyCard.tsx
+++ src/app/components/DummyCard/DummyCard.tsx
@@ -5,6 +5,9 @@ import 'app/components/DummyCard/DummyCard.css';
 interface CardType {
   card: {
     id: number;
+    category: string;
+    author: string;
+    rateCount: number;
     image: string;
     title: string;
     content: string;
@@ -15,21 +18,34 @@ interface CardType {
 
 const DummyCard = (props: CardType) => {
   const [showAllTags, setShowAllTags] = useState(false);
-  const { id, image, title, content, rate, tags } = props.card;
-  const isMoreThanTwo = tags.length > 2;
-  const moreTagsNum = tags.length - 2;
+  const { id, category, author, rateCount, image, title, content, rate, tags } =
+    props.card;
+  const isMoreThanTwo = tags.length > 3;
+  const moreTagsNum = tags.length - 3;
 
   return (
     <div className="card" title={title}>
       <Link to={`/lecture/${id}`} className="preview-link">
         <img src={image} className="card-img-top" alt="강의 샘플 이미지" />
         <div className="card-body">
+          <h5 className="card-title">
+            <span className="category">[{category}]</span>
+            {title}
+          </h5>
           <div className="rate-box">
             <img src="/images/rate.png" alt="평점" className="rate-img" />
-            <div className="rate-text">{rate.toFixed(1)}</div>
+            <div className="rate-text">
+              {rate.toFixed(1)}
+              <span className="rate-count">({rateCount})</span>
+            </div>
+            <img
+              src="/images/middle-line.png"
+              alt="구분선"
+              className="middle-line"
+            />
+            <div className="author-name">{author}</div>
           </div>
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{content}</p>
+          {/* <p className="card-text">{content}</p> */}
         </div>
       </Link>
       <div className="tags">
@@ -39,17 +55,19 @@ const DummyCard = (props: CardType) => {
                 {tag}
               </div>
             ))
-          : tags.slice(0, 2).map((tag) => (
+          : tags.slice(0, 3).map((tag) => (
               <div key={tag} className="lecture-tag">
                 {tag}
               </div>
             ))}
         {isMoreThanTwo && (
-          <div
-            className="more-tag-btn"
-            onClick={() => setShowAllTags(!showAllTags)}
-          >
-            {!showAllTags && '+' + moreTagsNum}
+          <div className="more-tag-container">
+            <div
+              className="more-tag-btn"
+              onClick={() => setShowAllTags(!showAllTags)}
+            >
+              {!showAllTags && '+' + moreTagsNum}
+            </div>
           </div>
         )}
       </div>
